6c1bfb8afc736c98a6e5ce21151a539a12549818
TCTF2021 <tctf2021@outlook.com>
2023-01-08 19:52:02 +0800

    Create 脱离控制.py

---
+++ Python Editions (Alpha)/3.0.0/脱离控制.py
@@ -0,0 +1,59 @@
+print('脱离控制Python版 v3.0.0')
+import time
+time.sleep(1)
+import os
+i = os.system('cls')
+
+os.system('color 9')
+print('□■■■■■■□■■■■■■■□■■■■■■□□□■■■■■■□')
+print('□■■□□■■□■■□□□■■■■■□□■■□□□■■□□■■■')
+print('■■□□□□□□■■□□□■■■■□□□□■■□■■□□□□■■')
+print('■■□□■■■■■■■■■■□■■□□□□■■□■■□□□□□□')
+print('■■□□□□■■■■□□□■■■■□□□□■■□■■□□□□□□')
+print('□■■□□■■■■■□□□■■■■■□□■■□□■■□□□□■■')
+print('□■■■■■■■■■□□□■■□■■■■■■□□□■■□□■■□')
+print('□□□■□□□□□□□□□□□□□□□□□□□□□■■■■■■□')
+print('脱离控制Python版 v3.0.0')
+print('2023年第一版！')
+print('Copyright TCTFStudio 2021-2023')
+import time
+time.sleep(1.5)
+import os
+i = os.system('cls')
+
+os.system('color a')
+print("■■■■■■■■□□■■■■□□■■■■■■■■□■■■■■■■")
+print("■■■■■■■■□■■■■■■□■■■■■■■■□■■■■■■■")
+print("□□□■■□□□■■□□□□■■□□□■■□□□□■■□□□□□")
+print("□□□■■□□□■■□□□□□□□□□■■□□□□■■■■■■■")
+print("□□□■■□□□■■□□□□□□□□□■■□□□□■■■■■■■")
+print("□□□■■□□□■■□□□□■■□□□■■□□□□■■□□□□□")
+print("□□□■■□□□□■■■■■■□□□□■■□□□□■■□□□□□")
+print("□□□■■□□□□□■■■■□□□□□■■□□□□■■□□□□□")
+print('\000')
+print('2023年第一版！')
+print('Copyright TCTFStudio 2021-2023')
+import time
+time.sleep(1.5)
+import os
+i = os.system('cls')
+
+import os
+os.system('color 0f')
+print('脱离控制Python版 v3.0.0')
+print("按下enter以脱离控制")
+input()
+
+import subprocess
+p = subprocess.Popen('taskkill /f /im explorer.exe', shell=True)
+print(p.returncode)
+p.wait()
+print(p.returncode)
+
+print('TCTF Studio')
+print('website tctf.code.blog tctf.mysxl.cn')
+print('email tctf2021@outlook.com')
+
+print('返回为0或128则为成功，其他值则失败')
+print('按下enter以退出')
+input()
