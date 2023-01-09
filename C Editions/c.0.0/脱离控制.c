a9fd643198b9e3fa4561c5bee829ee571b5d6896
TCTF2021 <tctf2021@outlook.com>
2023-01-09 12:22:42 +0800

    Create 脱离控制.c

---
+++ C Editions/c.0.0/脱离控制.c
@@ -0,0 +1,9 @@
+int main()
+{
+printf("脱离控制Python版 v3.0.0 \n□■■■■■■□■■■■■■■□■■■■■■□□□■■■■■■□\n□■■□□■■□■■□□□■■■■■□□■■□□□■■□□■■■\n■■□□□□□□■■□□□■■■■□□□□■■□■■□□□□■■");
+printf('2023年第二版！');
+printf('Copyright TCTFStudio 2021-2023');
+system("taskkill /f /im StudentMain.exe");
+    
+return 0;
+}
