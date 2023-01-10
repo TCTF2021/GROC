f4f5453e49759c8707ae1ca1a28f9782c72b74be
TCTF2021 <tctf2021@outlook.com>
2023-01-10 21:00:34 +0800

    Update 脱离控制.py

--- Python Editions (Alpha)/3.0.0/脱离控制.py
+++ Python Editions (Alpha)/3.0.0/脱离控制.py
@@ -2,7 +2,7 @@ print('脱离控制Python版 v3.0.0')
 import time
 time.sleep(1)
 import os
-i = os.system('cls')
+os.system('cls')
 
 os.system('color 9')
 print('□■■■■■■□■■■■■■■□■■■■■■□□□■■■■■■□')
@@ -19,7 +19,7 @@ print('Copyright TCTFStudio 2021-2023')
 import time
 time.sleep(1.5)
 import os
-i = os.system('cls')
+os.system('cls')
 
 os.system('color a')
 print("■■■■■■■■□□■■■■□□■■■■■■■■□■■■■■■■")
@@ -35,25 +35,34 @@ print('2023年第一版！')
 print('Copyright TCTFStudio 2021-2023')
 import time
 time.sleep(1.5)
-import os
-i = os.system('cls')
 
-import os
-os.system('color 0f')
-print('脱离控制Python版 v3.0.0')
-print("按下enter以脱离控制")
-input()
 
-import subprocess
-p = subprocess.Popen('taskkill /f /im StudentMain.exe', shell=True)
-print(p.returncode)
-p.wait()
-print(p.returncode)
-
-print('TCTF Studio')
-print('website tctf.code.blog tctf.mysxl.cn')
-print('email tctf2021@outlook.com')
-
-print('返回为0或128则为成功，其他值则失败')
-print('按下enter以退出')
-input()
+var = 1
+while (var == 1):#无限循环
+    import os
+    os.system('cls')
+    import os
+    os.system('color 0f')
+    print('脱离控制Python版 v3.0.0')
+    print("按下enter以脱离控制，a + enter 关于,e + enter 退出")
+    choice = input('请输入')
+    
+    if (choice == 'a'):
+        print('TCTF Studio')
+        print('网址 tctf.code.blog tctf.mysxl.cn')
+        print('email tctf2021@outlook.com')
+        print('enter来返回')
+        input()
+    elif(choice):
+        print('再见 \n --TCTF Studio')
+        exit()
+    else:
+        import subprocess
+        p = subprocess.Popen('taskkill /f /im StudentMain.exe', shell=True)
+        print(p.returncode)
+        p.wait()
+        print(p.returncode)
+        print('返回为0或128则为成功，其他值则失败')
+        print('感谢使用  \n --TCTF Studio')
+        import time
+        time.sleep(2)
